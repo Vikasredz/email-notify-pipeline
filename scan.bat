e06c1032754dbbbf52db183a2d4bff65be784669
Vikasredz <vikasreddy2700@gmail.com>
2025-05-23 09:14:49 +1000

    Update scan.bat

--- scan.bat
+++ scan.bat
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 @echo off
 echo Performing security scan...
 timeout /t 2 >nul
 echo No vulnerabilities found.
-=======
-#!/bin/bash
-echo "Performing security scan..."
-sleep 2
-echo "No vulnerabilities found."
->>>>>>> 51af81a (new changes)
+
