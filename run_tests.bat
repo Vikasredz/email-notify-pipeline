00a2a0dada762f692362e0409c63a082c5008283
Vikasredz <vikasreddy2700@gmail.com>
2025-05-23 09:14:16 +1000

    Update run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 @echo off
 echo Running tests...
 timeout /t 2 /nobreak >nul
 echo All tests passed!
-=======
-#!/bin/bash
-echo "Running tests..."
-sleep 2
-echo "All tests passed!"
->>>>>>> 51af81a (new changes)
